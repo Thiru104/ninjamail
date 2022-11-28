c171ca8abf6d8c56f06e67417bca861801daaf0b
Thirumurugan M <82165024+Thiru104@users.noreply.github.com>
2022-11-28 16:44:36 +0530

    Add files via upload
    changes

---
+++ fordesk/src/pages/NinjamailDesktop.tsx
@@ -0,0 +1,179 @@
+import { FunctionComponent } from "react";
+import "./NinjamailDesktop.css";
+
+const NinjamailDesktop: FunctionComponent = () => {
+  return (
+    <div className="ninjamail-desktop">
+      <img className="bg-stuff-icon2" alt="" src="../bg-stuff2@2x.png" />
+      <div className="footer-div2">
+        <div className="links2-div2">
+          <b className="privacy-policy-b2">Privacy Policy</b>
+          <b className="terms-conditions2">{`Terms & Conditions`}</b>
+        </div>
+        <b className="ninjamail-is-a-sample-project2">
+          NinjaMail is a sample project for Quest AI. © 2019 Quest AI, All
+          rights reserved.
+        </b>
+        <div className="divider2" />
+        <div className="footer-links-div2">
+          <b className="jobs2">Jobs</b>
+          <b className="slack-b2">Slack</b>
+          <b className="contact-us-b2">Contact Us</b>
+          <b className="linkedin-b2">LinkedIn</b>
+          <b className="instagram-b2">Instagram</b>
+          <b className="facebook2">Facebook</b>
+          <b className="twitter-b2">Twitter</b>
+          <b className="medium-b2">Medium</b>
+          <b className="case-studies-b2">Case Studies</b>
+          <b className="templates-b2">Templates</b>
+          <b className="help-center-b2">Help Center</b>
+          <b className="resources-b2">Resources</b>
+          <b className="tutorials-b2">Tutorials</b>
+          <b className="about-us2">About Us</b>
+          <b className="partners-b2">Partners</b>
+          <b className="services-b2">Services</b>
+          <b className="pricing-b2">Pricing</b>
+          <b className="features-b2">Features</b>
+        </div>
+        <div className="ninjamail-logo-div4">
+          <div className="ninjamail-div4">NinjaMail</div>
+          <img className="mail-icon4" alt="" src="../mail4@2x.png" />
+        </div>
+      </div>
+      <div className="get-started-div2">
+        <div className="getstarted-box-div2" />
+        <div className="pick-btn-div4">
+          <div className="pick-btn-div5" />
+          <div className="pick-a-plan2">PICK A PLAN</div>
+        </div>
+        <div className="get-started-today2">Get started today!</div>
+      </div>
+      <div className="sec5-div2">
+        <img className="logos-icon2" alt="" src="../logos2@2x.png" />
+        <div className="all-the-best-brands-already-us2">
+          <p className="all-the-best2">All the best brands</p>
+          <p className="already-use-us2">already use us.</p>
+        </div>
+      </div>
+      <div className="sec4-div2">
+        <div className="input-div2">
+          <div className="join-btn-div2" />
+          <div className="join-our-list2">JOIN OUR LIST</div>
+          <div className="email-input-box2" />
+          <div className="enter-your-email2">Enter your email</div>
+          <div className="thanks-email-received2">Thanks! Email received.</div>
+        </div>
+        <div className="learn-how-others-are-reaching2">
+          Learn how others are reaching their audience easier than ever before.
+        </div>
+        <div className="casestudy3-div2">
+          <img className="photo-icon10" alt="" src="../photo10@2x.png" />
+          <div className="photo-gradient-div6" />
+          <div className="elayne-div2">Elayne</div>
+          <b className="member-since-20182">Member since 2018</b>
+        </div>
+        <div className="casestudy2-div2">
+          <img className="photo-icon11" alt="" src="../photo11@2x.png" />
+          <div className="photo-gradient-div7" />
+          <div className="camile-div4">Camile</div>
+          <b className="member-since-20124">Member since 2012</b>
+        </div>
+        <div className="casestudy1-div2">
+          <img className="photo-icon11" alt="" src="../photo12@2x.png" />
+          <div className="photo-gradient-div7" />
+          <div className="camile-div4">Frankie</div>
+          <b className="member-since-20124">Member since 2016</b>
+        </div>
+      </div>
+      <div className="sec3-div2">
+        <div className="sec3-txt-div2">
+          <div className="whether-youre-a-startup-smal2">
+            <p className="all-the-best2">Whether you’re a startup, small</p>
+            <p className="all-the-best2">{`business, e-commerce store, or `}</p>
+            <p className="all-the-best2">want to promote your app, NinjaMail</p>
+            <p className="all-the-best2">has the feature set tailored for</p>
+            <p className="all-the-best2">{`your business. `}</p>
+          </div>
+          <div className="the-source-for-proven-engagin2">
+            <p className="all-the-best2">The source for</p>
+            <p className="all-the-best2">{`proven, engaging `}</p>
+            <p className="already-use-us2">email campaigns</p>
+          </div>
+        </div>
+        <div className="thumb2-div2">
+          <img className="photo-icon13" alt="" src="../photo13@2x.png" />
+          <div className="white-box-div4" />
+          <div className="learn-more4">{`Learn More >`}</div>
+          <div className="start-building-and-sharing-wit2">
+            Start building and sharing with your team today. NinjaMail is
+            renowned for its industry leading team collaboration tools.
+          </div>
+        </div>
+        <div className="thumb1-div2">
+          <img className="photo-icon13" alt="" src="../photo14@2x.png" />
+          <div className="white-box-div5" />
+          <div className="learn-more5">{`Learn More >`}</div>
+          <div className="launch-campaigns-but-also-find2">
+            Launch campaigns but also find new customers. Our unique platform
+            handles campaigns from start to end.
+          </div>
+        </div>
+      </div>
+      <div className="video-div2">
+        <img className="video-icon2" alt="" src="../video2@2x.png" />
+        <div className="learnhow-btn-div2">
+          <div className="learn-btn-div2" />
+          <div className="learn-how-div2">LEARN HOW</div>
+        </div>
+        <div className="reach-more-customers2">Reach More Customers</div>
+      </div>
+      <div className="hero-div2">
+        <img className="hero-photo-icon2" alt="" src="../hero-photo2@2x.png" />
+        <div className="hero-left-side-div2">
+          <div className="trynow-off-div2">
+            <div className="try-now-btn2" />
+            <div className="try-now-div2">TRY NOW</div>
+          </div>
+          <div className="getdemobtn-off-div2">
+            <div className="get-demo-btn2" />
+            <div className="get-a-demo2">GET A DEMO</div>
+          </div>
+          <div className="create-and-launch-email-campai2">
+            <p className="all-the-best2">
+              Create and launch email campaigns that captivate
+            </p>
+            <p className="already-use-us2">
+              your customers in just a few clicks.
+            </p>
+          </div>
+          <div className="create-stunning-email-campaig1">
+            <p className="all-the-best2">{`Create Stunning `}</p>
+            <p className="already-use-us2">Email Campaigns</p>
+          </div>
+        </div>
+      </div>
+      <div className="nav-div2">
+        <div className="links-div2">
+          <div className="signup-btn-div4">
+            <div className="signup-btn-div5" />
+            <div className="sign-up-free2">SIGN UP FREE</div>
+          </div>
+          <div className="underlink-1-div1" />
+          <div className="underlink-2-div1" />
+          <div className="underlink-3-div1" />
+          <div className="underlink-4-div1" />
+          <div className="partners-div1">PARTNERS</div>
+          <div className="services-div1">SERVICES</div>
+          <div className="pricing-div1">PRICING</div>
+          <div className="features-div1">FEATURES</div>
+        </div>
+        <div className="ninjamail-logo-div5">
+          <div className="ninjamail-div5">NinjaMail</div>
+          <img className="mail-icon5" alt="" src="../mail5@2x.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NinjamailDesktop;
---
+++ fordesk/src/pages/NinjamailTablet.tsx
@@ -0,0 +1,179 @@
+import { FunctionComponent } from "react";
+import "./NinjamailTablet.css";
+
+const NinjamailTablet: FunctionComponent = () => {
+  return (
+    <div className="ninjamail-tablet">
+      <img className="bg-stuff-icon1" alt="" src="../bg-stuff1@2x.png" />
+      <div className="footer-div1">
+        <div className="links2-div1">
+          <b className="terms-conditions1">{`Terms & Conditions`}</b>
+          <b className="privacy-policy-b1">Privacy Policy</b>
+        </div>
+        <b className="ninjamail-is-a-sample-project1">
+          NinjaMail is a sample project for Quest AI. © 2019 Quest AI, All
+          rights reserved.
+        </b>
+        <div className="divider1" />
+        <div className="footer-links-div1">
+          <b className="jobs1">Jobs</b>
+          <b className="slack-b1">Slack</b>
+          <b className="contact-us-b1">Contact Us</b>
+          <b className="linkedin-b1">LinkedIn</b>
+          <b className="instagram-b1">Instagram</b>
+          <b className="facebook1">Facebook</b>
+          <b className="twitter-b1">Twitter</b>
+          <b className="medium-b1">Medium</b>
+          <b className="case-studies-b1">Case Studies</b>
+          <b className="templates-b1">Templates</b>
+          <b className="help-center-b1">Help Center</b>
+          <b className="resources-b1">Resources</b>
+          <b className="tutorials-b1">Tutorials</b>
+          <b className="about-us1">About Us</b>
+          <b className="partners-b1">Partners</b>
+          <b className="services-b1">Services</b>
+          <b className="pricing-b1">Pricing</b>
+          <b className="features-b1">Features</b>
+        </div>
+        <div className="ninjamail-logo-div2">
+          <div className="ninjamail-div2">NinjaMail</div>
+          <img className="mail-icon2" alt="" src="../mail@2x.png" />
+        </div>
+      </div>
+      <div className="get-started-div1">
+        <div className="getstarted-box-div1" />
+        <div className="pick-btn-div2">
+          <div className="pick-btn-div3" />
+          <div className="pick-a-plan1">PICK A PLAN</div>
+        </div>
+        <div className="get-started-today1">Get started today!</div>
+      </div>
+      <div className="sec5-div1">
+        <img className="logos-icon1" alt="" src="../logos1@2x.png" />
+        <div className="all-the-best-brands-already-us1">
+          <p className="all-the-best1">All the best brands</p>
+          <p className="your-customers-in">already use us.</p>
+        </div>
+      </div>
+      <div className="sec4-div1">
+        <div className="input-div1">
+          <div className="join-btn-div1" />
+          <div className="join-our-list1">JOIN OUR LIST</div>
+          <div className="email-input-box1" />
+          <div className="enter-your-email1">Enter your email</div>
+          <div className="thanks-email-received1">Thanks! Email received.</div>
+        </div>
+        <div className="learn-how-others-are-reaching1">
+          Learn how others are reaching their audience easier than ever before.
+        </div>
+        <div className="casestudy3-div1">
+          <img className="photo-icon5" alt="" src="../photo@2x.png" />
+          <div className="photo-gradient-div3" />
+          <div className="elayne-div1">Elayne</div>
+          <b className="member-since-20181">Member since 2018</b>
+        </div>
+        <div className="casestudy2-div1">
+          <img className="photo-icon6" alt="" src="../photo6@2x.png" />
+          <div className="photo-gradient-div4" />
+          <div className="camile-div2">Camile</div>
+          <b className="member-since-20122">Member since 2012</b>
+        </div>
+        <div className="casestudy1-div1">
+          <img className="photo-icon6" alt="" src="../photo7@2x.png" />
+          <div className="photo-gradient-div4" />
+          <div className="camile-div2">Frankie</div>
+          <b className="member-since-20122">Member since 2016</b>
+        </div>
+      </div>
+      <div className="sec3-div1">
+        <div className="sec3-txt-div1">
+          <div className="whether-youre-a-startup-smal1">
+            <p className="all-the-best1">Whether you’re a startup, small</p>
+            <p className="all-the-best1">{`business, e-commerce store, or `}</p>
+            <p className="all-the-best1">want to promote your app, NinjaMail</p>
+            <p className="all-the-best1">has the feature set tailored for</p>
+            <p className="all-the-best1">{`your business. `}</p>
+          </div>
+          <div className="the-source-for-proven-engagin1">
+            <p className="all-the-best1">The source for</p>
+            <p className="all-the-best1">{`proven, engaging `}</p>
+            <p className="your-customers-in">email campaigns</p>
+          </div>
+        </div>
+        <div className="thumb2-div1">
+          <img className="photo-icon8" alt="" src="../photo8@2x.png" />
+          <div className="white-box-div2" />
+          <div className="learn-more2">{`Learn More >`}</div>
+          <div className="start-building-and-sharing-wit1">
+            Start building and sharing with your team today. NinjaMail is
+            renowned for its industry leading team collaboration tools.
+          </div>
+        </div>
+        <div className="thumb1-div1">
+          <img className="photo-icon9" alt="" src="../photo9@2x.png" />
+          <div className="white-box-div3" />
+          <div className="learn-more3">{`Learn More >`}</div>
+          <div className="launch-campaigns-but-also-find1">
+            Launch campaigns but also find new customers. Our unique platform
+            handles campaigns from start to end.
+          </div>
+        </div>
+      </div>
+      <div className="video-div1">
+        <img className="video-icon1" alt="" src="../video1@2x.png" />
+        <div className="learnhow-btn-div1">
+          <div className="learn-btn-div1" />
+          <div className="learn-how-div1">LEARN HOW</div>
+        </div>
+        <div className="reach-more-customers1">Reach More Customers</div>
+      </div>
+      <div className="hero-div1">
+        <img className="hero-photo-icon1" alt="" src="../hero-photo1@2x.png" />
+        <div className="hero-left-side-div1">
+          <div className="getdemobtn-off-div1">
+            <div className="get-demo-btn1" />
+            <div className="get-a-demo1">GET A DEMO</div>
+          </div>
+          <div className="trynow-off-div1">
+            <div className="try-now-btn1" />
+            <div className="try-now-div1">TRY NOW</div>
+          </div>
+          <div className="create-and-launch-email-campai1">
+            <p className="all-the-best1">
+              Create and launch email campaigns that captivate
+            </p>
+            <p className="your-customers-in">
+              your customers in just a few clicks.
+            </p>
+          </div>
+          <div className="create-stunning-email-campaig">
+            <p className="all-the-best1">{`Create Stunning `}</p>
+            <p className="your-customers-in">Email Campaigns</p>
+          </div>
+        </div>
+      </div>
+      <div className="nav-div1">
+        <div className="links-div1">
+          <div className="signup-btn-div2">
+            <div className="signup-btn-div3" />
+            <div className="sign-up-free1">SIGN UP FREE</div>
+          </div>
+          <div className="underlink-4-div" />
+          <div className="underlink-3-div" />
+          <div className="underlink-2-div" />
+          <div className="underlink-1-div" />
+          <div className="partners-div">PARTNERS</div>
+          <div className="services-div">SERVICES</div>
+          <div className="pricing-div">PRICING</div>
+          <div className="features-div">FEATURES</div>
+        </div>
+        <div className="ninjamail-logo-div3">
+          <div className="ninjamail-div3">NinjaMail</div>
+          <img className="mail-icon3" alt="" src="../mail3@2x.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NinjamailTablet;
